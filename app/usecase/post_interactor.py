03bf515e98c0e644a9d827bb42d1414d683fb661
bon <bontyun@gmail.com>
2023-07-17 23:30:49 +0900

    fix message converter

--- app/usecase/post_interactor.py
+++ app/usecase/post_interactor.py
@@ -1,5 +1,6 @@
 import logging
 
+from app.domain.services.message_converter import MessageConverter
 from app.infrastructure.post_repository import PostRepository
 
 
@@ -16,7 +17,12 @@ class PostInteractor:
 
     def get_posts_by_page(self, page_number, posts_per_page):
         post_repository = PostRepository()
-        return post_repository.find_by_page(page_number, posts_per_page)
+
+        posts = post_repository.find_by_page(page_number, posts_per_page)
+        for post in posts:
+            converted_message = MessageConverter.convert_message(post.content)
+            post.content = converted_message
+        return posts
 
     def get_total_post_count(self):
         post_repository = PostRepository()
