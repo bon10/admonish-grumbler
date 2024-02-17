148ff08db3aafaab396c01a6bb1dfc5a21637bdb
bon <bontyun@gmail.com>
2024-02-18 03:06:10 +0900

    fix ogp generate

--- app/domain/services/post_service.py
+++ app/domain/services/post_service.py
@@ -6,18 +6,25 @@ class PostService:
     @staticmethod
     def get_opengraph_data(url):
         try:
-            response = requests.get(url)
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+            }
+            response = requests.get(url, headers=headers)
             encoding = response.apparent_encoding  # レスポンスからエンコーディングを取得
             response.encoding = encoding  # レスポンスのエンコーディングを設定
             soup = BeautifulSoup(response.text, "html.parser")
             og_title = soup.find("meta", property="og:title")
             og_description = soup.find("meta", property="og:description")
             og_image = soup.find("meta", property="og:image")
-            return {
-                "title": og_title["content"] if og_title else None,
-                "description": og_description["content"] if og_description else None,
-                "image": og_image["content"] if og_image else None,
-            }
+            if og_title and og_description and og_image:
+                return {
+                    "title": og_title["content"],
+                    "description": og_description["content"],
+                    "image": og_image["content"],
+                }
+            else:
+                # OGPが存在しない場合はNoneを返す
+                return None
         except Exception as e:
             print(f"Error fetching OpenGraph data: {e}")
             return None
